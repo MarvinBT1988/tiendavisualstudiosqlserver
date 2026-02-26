03956192e7a87affe3e2fcaff5a836cfdf57b7da
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2026-02-25 20:29:00 -0600

    Agregar archivos de proyecto.

---
+++ Tienda.AppMVC/Models/TiendaDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tienda.AppMVC.Models;
+
+public partial class TiendaDbContext : DbContext
+{
+    public TiendaDbContext()
+    {
+    }
+
+    public TiendaDbContext(DbContextOptions<TiendaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+  
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.CategoriaId).HasName("PK__Categori__F353C1C5EE16082D");
+
+            entity.Property(e => e.CategoriaId).HasColumnName("CategoriaID");
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.ProductoId).HasName("PK__Producto__A430AE8354EA8329");
+
+            entity.Property(e => e.ProductoId).HasColumnName("ProductoID");
+            entity.Property(e => e.CategoriaId).HasColumnName("CategoriaID");
+            entity.Property(e => e.FechaIngreso)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Categoria).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.CategoriaId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Categoria_Producto");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
